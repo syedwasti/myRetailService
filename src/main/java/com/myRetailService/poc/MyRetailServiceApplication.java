adb4868ede435f9aebeebd26e66a9dd43485d760
SyedWasti <mdwasti@gmail.com>
2021-02-09 20:50:54 -0800

    creating spring boot project with maven

---
+++ src/main/java/com/myRetailService/poc/MyRetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myRetailService.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myRetailService/poc/MyRetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myRetailService.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
